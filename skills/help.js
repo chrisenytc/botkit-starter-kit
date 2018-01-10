a6a247e21949eabb9d533e1a5565ea86a1ea27fe
Christopher Enytc <chris@enytc.com>
2018-01-10 12:45:27 -0200

    Update help skill

--- skills/help.js
+++ skills/help.js
@@ -1,3 +1,5 @@
+const Debug = require('debug')('botkit:help')
+
 module.exports = function help (controller) {
   // This before middleware allows the help command to accept sub-thread names as a parameter
   // so users can say help to get the default thread, but help <subthread> will automatically
@@ -5,12 +7,18 @@ module.exports = function help (controller) {
   controller.studio.before('help', (convo, next) => {
     // is there a parameter on the help command?
     // if so, change topic.
+
+    Debug('BEFORE: help')
+
     const Matches = convo.source_message.text.match(/^help (.*)/i)
 
     if (Matches) {
+      Debug('BEFORE: help => Matched')
       if (convo.hasThread(Matches[1])) {
+        Debug(`BEFORE: help => Matched thread ${Matches[1]}`)
         convo.gotoThread(Matches[1])
       }
+      Debug('BEFORE: help => Call next')
       next()
     }
   })
